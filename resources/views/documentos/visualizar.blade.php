9e2129adfc75acc0e907148fc6d55a061b4a31bd
MartinRamirez <martinramirezpando@gmail.com>
2018-12-05 12:06:43 -0500

    martinsaves

--- resources/views/documentos/visualizar.blade.php
+++ resources/views/documentos/visualizar.blade.php
@@ -4,16 +4,21 @@
 	
 
 <div class="col-3"><a class="btn btn-success" href="{{url('admin/alumnos/visualizar/').'/'.$documento->alumno->id}}">Volver Atrás</a></div>
-	<div class="col-6">
-
-			<img  src="{{asset('imgPublications/'.$documento->imgUrl)}}" class="img-fluid" alt="Responsive image">
-			<div style="text-align: center; margin-top: 20px;">
-				<a class="btn btn-success" href="{{url('admin/documentos/editar/'.$documento->id)}}" style="margin-right: 50px;">Editar Datos</a>
+	
+	
+<div class="col-6" style="margin-top: 6rem;">
+		<a href="{{asset('imgPublications/'.$documento->imgUrl)}}" target="_blank"> <img  src="{{asset('imgPublications/'.$documento->imgUrl)}}" class="img-fluid" alt="Responsive image" style="padding: 0rem;
+																																																 width: 600px;
+																																																 height: 300px;	
+																																																 border-radius: 10%;		"></a>
+			<div style="text-align: center; margin-top: 40px;">
+				<a class="btn btn-success" href="{{url('admin/documentos/editar/'.$documento->id)}}" style="margin-right: 90px;
+																											margin-top: 30px;					">Editar Datos</a>
 				<form class="inline" action="{{url('admin/documentos/eliminar')}}" method="POST">
-		  			@csrf<input type="hidden" name="id" value="{{$documento->id}}"><input class="btn btn-danger" type="submit" value="Eliminar Documento"></form>
+		  			@csrf<input type="hidden" name="id" value="{{$documento->id}}"><input class="btn btn-danger" type="submit" value="Eliminar Documento" style=" margin-top: 30px;"></form>
 			</div>
 			
-	</div>
+</div>
 	
 
 @endsection
